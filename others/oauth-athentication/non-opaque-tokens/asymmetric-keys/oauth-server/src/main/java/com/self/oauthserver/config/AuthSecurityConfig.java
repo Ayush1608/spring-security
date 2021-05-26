1d600b2f4f0326640224ce77cee1b7c49160ffeb
Ayush <ayush@quovantis.com>
2021-05-27 00:40:28 +0530

    Exposing public key from Auth server to resource server JWT

--- others/oauth-athentication/non-opaque-tokens/asymmetric-keys/oauth-server/src/main/java/com/self/oauthserver/config/AuthSecurityConfig.java
+++ others/oauth-athentication/non-opaque-tokens/asymmetric-keys/oauth-server/src/main/java/com/self/oauthserver/config/AuthSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -39,7 +40,19 @@ public class AuthSecurityConfig extends AuthorizationServerConfigurerAdapter {
            .withClient("client1")
            .secret("secret1")
            .authorizedGrantTypes("password")
-           .scopes("read");
+           .scopes("read")
+           .and()
+           .withClient("resource")
+           .secret("rssecret");
+  }
+
+  @Override public void configure(final AuthorizationServerSecurityConfigurer security) throws Exception {
+    /*
+     * Used to expose public key to whoever calls this API
+     * params -> isAuthenticated, permitAll()
+     * URL -> /oauth/token_key
+    */
+    security.tokenKeyAccess("isAuthenticated()");
   }
 
   @Bean
